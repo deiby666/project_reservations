0ca2d2f81a020f07afea8e8f644204d87048a49b
greendy666 <cardonadeiby88@gmail.com>
2024-07-28 15:36:11 -0500

    entities configured succesfully

--- src/modules/reservations/entities/reservation.entity.ts
+++ src/modules/reservations/entities/reservation.entity.ts
@@ -1,1 +1,12 @@
-export class Reservation {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class Reservation {
+    @Prop()
+    type: string;
+
+    @Prop()
+    userID: string;
+}   
+
+export const reservationSchema = SchemaFactory.createForClass(Reservation);
